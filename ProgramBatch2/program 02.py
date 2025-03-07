5226e831482ac96222664bcef0154021875667df
Erica Guillermo <ericajhaneg@gmail.com>
2025-03-08 00:33:54 +0800

    Update program 02.py

--- ProgramBatch2/program 02.py
+++ ProgramBatch2/program 02.py
@@ -1,3 +1,3 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 print ("not equal" if num1 != num2 else "equal")
