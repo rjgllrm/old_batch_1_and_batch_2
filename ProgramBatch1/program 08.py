21dcb7129023798a6e8d972fb9e8372f01bfcca5
Erica Guillermo <ericajhaneg@gmail.com>
2025-03-08 00:31:53 +0800

    Update program 08.py

--- ProgramBatch1/program 08.py
+++ ProgramBatch1/program 08.py
@@ -1,2 +1,2 @@
-nums = [int(input("Enter a number: ")) for _ in range(10)]
+nums = [float(input("Enter a number: ")) for _ in range(10)]
 print("Count of odd numbers:", sum(1 for n in nums if n % 2 != 0))
